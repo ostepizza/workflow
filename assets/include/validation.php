9a3f7d35229ab57965259ff97c4de53c5f5a2441
idiot <32636970+ostepizza@users.noreply.github.com>
2023-11-13 13:13:50 +0100

    Added a validator class
    Added a validator class
    Added methods for validating registration input (tos, email, password, names (first and last))

---
+++ assets/include/validation.php
@@ -0,0 +1,90 @@
+<?php
+class Validator {
+    public $feedback = array();
+    public $valid = false;
+
+    function __construct(){
+
+    }
+
+    function validateRegistration($tosCheckmark, $email, $password, $firstName, $lastName) {
+        $this->validateTosCheckmark($tosCheckmark);
+        $this->validateEmail($email);
+        $this->validatePassword($password);
+        $this->validateFirstName($firstName);
+        $this->validateLastName($lastName);
+    }
+
+    function validateTosCheckmark($tosCheckmark) {
+        if (!$tosCheckmark) {
+            array_push($this->feedback, 'You need to accept the terms & conditions.<br>');
+            $this->valid = false;
+            return;
+        }
+        $this->valid = true;
+    }
+
+    function validateEmail($email) {
+        if (!empty($email)) {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($this->feedback, $email . ' is not a valid email.<br>');
+                $this->valid = false;
+                return;
+            }
+        } else {
+            array_push($this->feedback, 'You need to enter an email.<br>');
+            $this->valid = false;
+            return;
+        }
+        $this->valid = true;
+    }
+
+    function validatePassword($password) {
+        if (!empty($password)) {
+            if(!preg_match('@[0-9]@', $password)) {
+                array_push($this->feedback, 'Password does not contain a number.<br>');
+                $this->valid = false;
+                return;
+            }
+            if (!preg_match('@[^\w]@', $password)) {
+                array_push($this->feedback, 'Password does not contain a special character.<br>');
+                $this->valid = false;
+                return;
+            }
+            if (strlen($password) < 8) {
+                array_push($this->feedback, 'Password is not at least 8 characters long.<br>');
+                $this->valid = false;
+                return;
+            }
+        } else {
+            array_push($this->feedback, 'You need to enter a password.<br>');
+            $this->valid = false;
+            return;
+        }
+        $this->valid = true;
+    }
+
+    function validateName($name, $nameType) {
+        if (!empty($name)) {
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                array_push($this->feedback, 'Only letters and white space allowed in ' . $nameType . ' name "' . $name . '".<br>');
+                $this->valid = false;
+                return;
+            }
+        } else {
+            array_push($this->feedback, 'You need to enter a ' . $nameType . ' name.<br>');
+            $this->valid = false;
+            return;
+        }
+        $this->valid = true;
+    }
+
+    function validateFirstName($firstName) {
+        $this->validateName($firstName, 'first');
+    }
+
+    function validateLastName($lastName) {
+        $this->validateName($lastName, 'last');
+    }
+}
+?>
