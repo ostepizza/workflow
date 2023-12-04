5e3b46a66a3b803716a41775d7fd92aa57685e5e
idiot <32636970+ostepizza@users.noreply.github.com>
2023-12-04 16:03:26 +0100

    Added some Application methods
    Added some Application methods to the DBHandler

--- assets/include/DBHandler.php
+++ assets/include/DBHandler.php
@@ -1009,6 +1009,93 @@ class DBHandlerListing extends DBHandlerCompany {
             return false;
         }
     }
-
 }
+
+class DBHandlerApplication extends DBHandlerBase {
+    // Creates a new application and returns the new application ID if successful. Else returns false.
+    function createNewApplication($listingId, $userId) {
+        // Create a new empty application
+        $sql = 'INSERT INTO `job_application` (`job_listing_id`, `user_id`) VALUES (?, ?)';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('ii', $listingId, $userId);
+
+        // If the statement executes, return the application id. Else return false.
+        if ($stmt->execute()) {
+            $applicationId = $this->conn->insert_id;
+            $stmt->close();
+            return $applicationId;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
+
+    // Retrieves all applications from a user id. Returns an array with the applications if successful, else returns false.
+    function getAllUserApplications($userId) {
+        $sql = 'SELECT ja.*, jl.name as listing_name, jl.company_id, jl.deadline, jl.published, jl.views, c.name as company_name, c.description as company_description
+            FROM `job_application` ja
+            JOIN `job_listing` jl ON a.listing_id = jl.id
+            JOIN `company` c ON jl.company_id = c.id
+            WHERE ja.`user_id` = ? 
+            ORDER BY jl.`published` DESC, jl.`deadline` IS NULL ASC, jl.`deadline` ASC';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('i', $userId);
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $applications = $result->fetch_all(MYSQLI_ASSOC);
+            $stmt->close();
+            return $applications;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
+
+    // Retrieves all applications from a listing id. Returns an array with the applications if successful, else returns false.
+    // Todo: Possibility to retrieve archived applications
+    function getAllListingApplications($listingId, $archived = false) {
+        $sql = 'SELECT ja.*, u.first_name, u.last_name, u.email, u.telephone, u.location, u.birthday, u.picture, u.cv, u.competence, c.name as company_name
+            FROM `job_application` ja
+            JOIN `user` u ON ja.user_id = u.id
+            JOIN `job_listing` jl ON ja.job_listing_id = jl.id
+            JOIN `company` c ON jl.company_id = c.id
+            WHERE ja.`job_listing_id` = ? AND ja.`sent` = 1 AND ja.`archived` = 0';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('i', $listingId);
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $applications = $result->fetch_all(MYSQLI_ASSOC);
+            $stmt->close();
+            return $applications;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
+
+    // Retrieves an application by application id. Returns an array with the application if successful, else returns false.
+    function getApplication($applicationId) {
+        $sql = 'SELECT ja.*, u.first_name, u.last_name, u.email, u.telephone, u.location, u.birthday, u.picture, u.cv, u.competence, c.name as company_name
+            FROM `job_application` ja
+            JOIN `user` u ON ja.user_id = u.id
+            JOIN `job_listing` jl ON ja.job_listing_id = jl.id
+            JOIN `company` c ON jl.company_id = c.id
+            WHERE ja.`id` = ?';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('i', $applicationId);
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $application = $result->fetch_assoc();
+            $stmt->close();
+            return $application;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
+}
+
 ?>
